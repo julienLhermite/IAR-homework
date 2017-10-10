ed435bbe2d678f4ea947d1bbd98396ccd2e8ed83
Julien Lhermite <julien.lhermite@gmail.com>
2017-10-11 01:46:16 +0200

    plot added

--- test.py
+++ test.py
@@ -1,10 +1,36 @@
-import itertools
+from pylab import *
+import time
 
-GRID_SIZE = (2,2)
-base_pos = [1, 0]
+import matplotlib.backends
+import matplotlib.pyplot as p
+import os.path
 
 
+def is_backend_module(fname):
+    """Identifies if a filename is a matplotlib backend module"""
+    return fname.startswith('backend_') and fname.endswith('.py')
 
-print(possible)
-print(possible_dirty_cells)
-print(len(possible_dirty_cells))
+def backend_fname_formatter(fname):
+    """Removes the extension of the given filename, then takes away the leading 'backend_'."""
+    return os.path.splitext(fname)[0][8:]
+
+# get the directory where the backends live
+backends_dir = os.path.dirname(matplotlib.backends.__file__)
+
+# filter all files in that directory to identify all files which provide a backend
+backend_fnames = filter(is_backend_module, os.listdir(backends_dir))
+
+backends = [backend_fname_formatter(fname) for fname in backend_fnames]
+
+print("supported backends: \t" + str(backends))
+
+# validate backends
+backends_valid = []
+for b in backends:
+    try:
+        p.switch_backend(b)
+        backends_valid += [b]
+    except:
+        continue
+
+print("valid backends: \t" + str(backends_valid))
